f1e1470d9d24f1d07ec1fe94f1d73fb900b825fb
vishnuas22 <vishnuofficial18@gmail.com>
2025-10-25 02:56:07 +0530

    Alpha Master 1 🎉☠️ 2025-10-25 02:56:07

--- frontend/src/services/api/index.ts
+++
@@ -1,7 +0,0 @@
-// API Service barrel export
-export { default as apiClient } from './client';
-export { authAPI } from './auth.api';
-export { chatAPI } from './chat.api';
-export { analyticsAPI } from './analytics.api';
-export { voiceAPI } from './voice.api';
-export { gamificationAPI } from './gamification.api';
--- frontend/src/store/index.ts
+++
@@ -1,5 +0,0 @@
-// Store barrel export
-export { useAuthStore } from './authStore';
-export { useChatStore } from './chatStore';
-export { useEmotionStore } from './emotionStore';
-export { useUIStore } from './uiStore';
