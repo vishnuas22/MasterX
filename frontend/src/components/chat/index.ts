06dd272a79f2c8a1f82f69d1cadc5b7ac29e0422
vishnuas22 <vishnuofficial18@gmail.com>
2025-10-27 11:56:46 +0530

    index.ts

---
+++ frontend/src/components/chat/index.ts
@@ -0,0 +1,23 @@
+/**
+ * Chat Components - Barrel Export
+ * 
+ * GROUP 9: Chat Interface (Core Feature)
+ * All chat-related components for the MasterX learning platform
+ */
+
+export { ChatContainer } from './ChatContainer';
+export { MessageList } from './MessageList';
+export { Message } from './Message';
+export { MessageInput } from './MessageInput';
+export { EmotionIndicator } from './EmotionIndicator';
+export { TypingIndicator } from './TypingIndicator';
+export { VoiceButton } from './VoiceButton';
+
+// Type exports
+export type { ChatContainerProps } from './ChatContainer';
+export type { MessageListProps } from './MessageList';
+export type { MessageProps } from './Message';
+export type { MessageInputProps } from './MessageInput';
+export type { EmotionIndicatorProps } from './EmotionIndicator';
+export type { TypingIndicatorProps } from './TypingIndicator';
+export type { VoiceButtonProps } from './VoiceButton';
