71aed71d5e0f9b22120000e50e7f3fa468eebfaa
vishnuas22 <vishnuofficial18@gmail.com>
2025-10-27 18:35:12 +0530

    index.ts for Emotions

---
+++ frontend/src/components/emotion/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Emotion Components - Barrel Exports
+ * 
+ * GROUP 10: Emotion Visualization Components
+ * Files 55-58/87
+ */
+
+export { EmotionWidget, default as EmotionWidgetDefault } from './EmotionWidget';
+export { EmotionChart, default as EmotionChartDefault } from './EmotionChart';
+export { EmotionTimeline, default as EmotionTimelineDefault } from './EmotionTimeline';
+export { MoodTracker, default as MoodTrackerDefault } from './MoodTracker';
+
+// Re-export types
+export type { EmotionWidgetProps } from './EmotionWidget';
+export type { EmotionChartProps } from './EmotionChart';
+export type { EmotionTimelineProps } from './EmotionTimeline';
+export type { MoodTrackerProps } from './MoodTracker';
