48f55542b1c17e066ba624746f6f186c449d9126
vishnuas22 <vishnuofficial18@gmail.com>
2025-10-28 15:58:57 +0530

    components index.ts added

---
+++ frontend/src/components/analytics/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Analytics Components - Barrel Export
+ * 
+ * Centralizes exports for all analytics-related components
+ * for cleaner imports throughout the application
+ */
+
+export { StatsCard } from './StatsCard';
+export type { StatsCardProps } from './StatsCard';
+
+export { ProgressChart } from './ProgressChart';
+export type { ProgressChartProps, ProgressDataPoint } from './ProgressChart';
+
+export { LearningVelocity } from './LearningVelocity';
+export type { LearningVelocityProps } from './LearningVelocity';
+
+export { TopicMastery } from './TopicMastery';
+export type { TopicMasteryProps, Topic } from './TopicMastery';
